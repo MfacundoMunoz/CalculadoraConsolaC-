b68bb4f232ddb582cd9e46e78f93814c26ad52c3
FacundoMuñoz <137367504+MfacundoMunoz@users.noreply.github.com>
2023-10-16 18:12:13 -0300

    Add files via upload

---
+++ CalculadoraConsola/CalculadoraConsola/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class CalculadoraConsola
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Calculadora Simple");
+            Console.WriteLine("Elige una operación:");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicación");
+            Console.WriteLine("4. División");
+            Console.WriteLine("5. Salir");
+
+            Console.Write("Opción: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            if (opcion == 5)
+            {
+                break; 
+            }
+
+            Console.Write("Ingrese el primer número: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (opcion)
+            {
+                case 1:
+                    resultado = numero1 + numero2;
+                    Console.WriteLine($"Resultado: {resultado}");
+                    break;
+                case 2:
+                    resultado = numero1 - numero2;
+                    Console.WriteLine($"Resultado: {resultado}");
+                    break;
+                case 3:
+                    resultado = numero1 * numero2;
+                    Console.WriteLine($"Resultado: {resultado}");
+                    break;
+                case 4:
+                    if (numero2 != 0)
+                    {
+                        resultado = numero1 / numero2;
+                        Console.WriteLine($"Resultado: {resultado}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: No se puede dividir por cero.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        }
+    }
+}
